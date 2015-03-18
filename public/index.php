53d75e5f1f63e1ccd07cf0e3e09fccfe9fc30e41
outadoc <outadoc@gmail.com>
2015-03-18 11:30:58 +0100

    Fix database name (oops)

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ $app->di->set('db', function(){
 			"host" => "127.0.0.1",
 			"username" => "root",
 			"password" => "",
-			"dbname" => "openbeer",
+			"dbname" => "open-beer",
 			"options" => array(
 					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 			)
